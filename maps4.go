176ca0bb49a374792fe1dfc27af2fbfbcc471767
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-05-04 16:43:01 +0430

    last commit

---
+++ maps4.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	m := make(map[string]int)
+
+	m["k1"] = 7
+
+	m["k2"] = 13
+
+	delete(m, "k2")
+
+	if _, ok := m["k2"]; ok {
+
+		fmt.Println("Ok")
+
+	} else {
+
+		fmt.Println("NotOk")
+
+	}
+
+}
