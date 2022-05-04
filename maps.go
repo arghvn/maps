cd7b80f1955733aff3575138f11dc3d42664251f
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-05-04 15:08:33 +0430

    first commit

---
+++ maps.go
@@ -0,0 +1,18 @@
+package main
+
+//maps : a A map is a set of value-key data with a type data associative
+//structure similar to a dictionary in Python.
+//space for more explain for above sentences
+
+//In Go, a map is a powerful, ingenious, and versatile data structure.
+//Golang Maps is a collection of unordered pairs of key-value.
+//It is widely used because it provides fast lookups and values that can retrieve, update or delete with the help of keys.
+
+import "fmt"
+
+func main() {
+
+//To create an empty map, use the 'make' :
+//make(map[key-type]val-type)	
+
+
